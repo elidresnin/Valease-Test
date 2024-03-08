458264a467d9c7148e23224547b87b9dd131fa98
Eli Dresnin <s018305@students.lmsd.org>
2024-03-08 11:58:18 -0500

    Initial Commit

---
+++ Valease/ValeaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValeaseApp.swift
+//  Valease
+//
+//  Created by Eli Dresnin (student LM) on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ValeaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
