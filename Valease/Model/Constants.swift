ae67cdd77ec7ba9df2658d321373466f3afa7d13
Penny Cohen <rosepenny2007@gmail.com>
2024-03-11 14:17:40 -0400

    constants

---
+++ Valease/Model/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Valease
+//
+//  Created by Penelope Cohen (student LM) on 3/11/24.
+//
+
+import SwiftUI
+
+extension Color{
+    static let background = Color("background")
+    static let textColor = Color("textColor")
+    static let valeaseGreen = Color("valeaseGreen")
+    static let valeaseOrange = Color("valeaseOrange")
+    static let valeaseTeal = Color("valeaseTeal")
+    
+}
+
+struct Constants{
+    static let largeFont: Font = Font(UIFont.systemFont(ofSize: 50))
+    
+    static let mediumFont: Font = Font(UIFont.systemFont(ofSize: 30))
+    
+    static let smallFont: Font = Font(UIFont.systemFont(ofSize: 15))
+}
